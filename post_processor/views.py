b705ca27d36982d39dc6e45fb02feac6d1d54c72
Burak Ufuktepe <buu837@g.harvard.edu>
2023-04-16 07:31:05 -0700

    Finished debugging.

--- post_processor/views.py
+++ post_processor/views.py
@@ -24,29 +24,25 @@ def test(request):
 @csrf_exempt
 def handle_request(request):
     """
-    Route the request.
+    Create workers to merge the results and create the results csv file.
     """
-    logger.info(f'Received {request.method} request.')
-    print(request.method)
+    logger.debug(f'Received {request.method} request.')
+
     if request.method != 'POST':
         logger.info('Received invalid request.')
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
-    print('1')
     request_body = json.loads(request.body)
-
-    print('2')
-    logger.info(f'Received {request.method} request.')
     run_ids_str = request_body.get('run_ids', None)
-    print('3')
+
     if run_ids_str is None:
-        print('Invalid run IDs.')
+        logger.error(f'Missing run ID in request.')
         return Response(status=status.HTTP_400_BAD_REQUEST)
-    print('4')
+
     run_ids = run_ids_str.split()
-    print(f'Run ids are: {run_ids}')
+
     feature_table_paths, taxonomy_results_paths = get_file_paths(run_ids)
-    print('6')
+
     timestamp = strftime('%Y%m%d-%H%M%S')
 
     try:
@@ -65,7 +61,7 @@ def handle_request(request):
                              'create_csv_task_id': worker_create_results_csv.task_id,
                              'timestamp': timestamp})
 
-    response.headers['Access-Control-Allow-Origin'] = '*'
+    # response.headers['Access-Control-Allow-Origin'] = '*'
 
     return response
 
@@ -74,31 +70,33 @@ def get_file_paths(run_ids):
     """
     Return file path strings for feature tables and taxonomy results
     """
-    print(f'get_file_paths {10}')
     feature_table_paths = ''
     taxonomy_results_paths = ''
 
     for run_id in run_ids:
         try:
-            print(f'get_file_paths {11}')
             run_status = Status.objects.get(pk=run_id)
-            print(f'get_file_paths {12}')
         except Status.DoesNotExist:
             # Skip the run id
             continue
 
         # Build file paths
         output_path = run_status.output_path
+
+        if not output_path:
+            # Skip the run id
+            continue
+
         feature_table_path = os.path.join(output_path, f'{run_id}_feature-table.qza')
         taxonomy_results_path = os.path.join(output_path, f'{run_id}_taxonomy.qza')
-        print(f'get_file_paths {13}')
+
         # Verify that files exist
         if not os.path.isfile(feature_table_path):
             continue
         if not os.path.isfile(taxonomy_results_path):
             continue
-        print(f'get_file_paths {14}')
+
         feature_table_paths += feature_table_path + ' '
         taxonomy_results_paths += taxonomy_results_path + ' '
-        print(f'get_file_paths {15}')
+
     return feature_table_paths, taxonomy_results_paths
